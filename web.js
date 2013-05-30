52bc9db69768667682698f006dbe3721d34574cc
Kim Ash <ash.kimberly.b@gmail.com>
2013-05-30 17:54:29 -0400

    added db and server config to web.js

--- web.js
+++ web.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var ejs = require('ejs'); //embedded javascript template engine
 var app = express.createServer(express.logger());
+var mongoose = require('mongoose');
+
+/************ DATABASE CONFIGURATION **********/
+app.db = mongoose.connect(process.env.MONGOLAB_URI); //connect to mongolabs db - local server uses .env
+
+// Include models.js - this file includes the database schema and defines the models used
+//require('./models').configureSchema(schema, mongoose);
+
+// Define your DB Model variables
+//var SixWordIt = mongoose.model('SixWordIt');
+//var Captions = mongoose.model('Captions');
+/************* END DATABASE CONFIGURATION *********/
+
+/*********** SERVER CONFIGURATION *****************/
+app.configure(function() {
+    
+    /*********************************************************************************
+        Configure the template engine
+        We will use EJS (Embedded JavaScript) https://github.com/visionmedia/ejs
+        
+        Using templates keeps your logic and code separate from your HTML.
+        We will render the html templates as needed by passing in the necessary data.
+    *********************************************************************************/
+
+    app.set('view engine','ejs');  // use the EJS node module
+    app.set('views',__dirname+ '/views'); // use /views as template directory
+    app.set('view options',{layout:true}); // use /views/layout.html to manage your main header/footer wrapping template
+    app.register('html',require('ejs')); //use .html files in /views
+
+    /******************************************************************
+        The /static folder will hold all css, js and image assets.
+        These files are static meaning they will not be used by
+        NodeJS directly. 
+        
+        In your html template you will reference these assets
+        as yourdomain.heroku.com/img/cats.gif or yourdomain.heroku.com/js/script.js
+    ******************************************************************/
+    app.use(express.static(__dirname + '/static'));
+    
+    //parse any http form post
+    app.use(express.bodyParser());
+    
+    /**** Turn on some debugging tools ****/
+    app.use(express.logger());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+});
 
 app.get('/', function(request, response) {
-  response.render('layout.html');
+  response.send('Hello world!');
 });
 
 var port = process.env.PORT || 3000;
